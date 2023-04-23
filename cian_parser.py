86b8a747ff06d8acb00fa82e2102a5f1aac28b2e
koluiree <66645706+koluiree@users.noreply.github.com>
2023-04-23 20:36:12 +0300

    Add files via upload

---
+++ cian_parser.py
@@ -0,0 +1,141 @@
+from typing import Union
+from bs4 import BeautifulSoup
+import cloudscraper
+import json
+from transliterate import translit
+
+
+class Parser:
+    RUBLE_SYMBOL = "₽"
+
+    def __init__(self, minprice: int = 0, maxprice: int = 0, region: str = "Москва", rooms: str = "1-6", page: int = 1):
+        self.rooms = rooms
+        self.min_price = minprice
+        self.max_price = maxprice
+        self.region = region
+        self.page = page
+        self.session = cloudscraper.create_scraper()
+        self.session.headers = {'Accept-Language': 'en'}
+        self.offers = []
+
+    def get_phone_number(self, link: str) -> Union[str, None]:
+
+        results = self.session.get(url=link)
+        results = results.text
+        offer_page = BeautifulSoup(results, "lxml")
+        try:
+            contact_data = offer_page.select("div[data-name='OfferContactsAside']")[0].text
+            return contact_data[:16]
+        except Exception:
+            return None
+
+
+    def get_rooms_count(self, text: str) -> int:
+        if "1-комн" in text or "1-к" in text:
+            rooms_count = 1
+        elif "2-комн" in text or "2-к" in text:
+            rooms_count = 2
+        elif "3-комн" in text or "3-к" in text:
+            rooms_count = 3
+        elif "4-комн" in text or "4-к" in text:
+            rooms_count = 4
+        elif "5-комн" in text or "5-к" in text:
+            rooms_count = 5
+        else:
+            rooms_count = -1
+
+        return rooms_count
+
+    def get_price(self, offer: BeautifulSoup) -> int:
+        price = -1
+        elements = offer.select("div[data-name='LinkArea']")[0].select(
+            "div[data-name='GeneralInfoSectionRowComponent']"
+        )
+        for element in elements:
+            if self.RUBLE_SYMBOL in element.text:
+                price_description = element.text
+                price = int("".join(price_description[: price_description.find(self.RUBLE_SYMBOL) - 1].split()))
+        return price
+
+    def parse_listing(self, offer):
+        offer_info = dict()
+        offer_info["link"] = offer.select("div[data-name='LinkArea']")[0].select("a")[0].get("href")
+        offer_info["price"] = self.get_price(offer)
+        offer_info["rooms"] = self.get_rooms_count(offer.text)
+        offer_info["city"] = translit(self.region.capitalize(), "ru", reversed=True)
+        offer_info["phone"] = self.get_phone_number(offer_info["link"])
+        self.offers.append(offer_info)
+
+    def start_session(self) -> list:
+        cities = {'Абакан': '4628', 'Анадырь': '4634', 'Анапа': '5129', 'Архангельск': '4557', 'Астрахань': '4558',
+                  'Барнаул': '4555', 'Белгород': '4561', 'Биробиджан': '4569', 'Благовещенск': '4556',
+                  'Бронницы': '4690', 'Брянск': '4562', 'Видный': '5922', 'Владивосток': '4604', 'Владикавказ': '4613',
+                  'Владимир': '4564', 'Волгоград': '4565', 'Вологда': '4566', 'Волоколамск': '5379', 'Воронеж': '4567',
+                  'Воскресенск': '5388', 'Геленджик': '4717', 'Горно-Алтайск': '4554', 'Грозный': '4631',
+                  'Дзержинский': '4734', 'Дмитров': '5482', 'Долгопрудный': '4738', 'Дубна': '4741',
+                  'Екатеринбург': '4612', 'Жуковский': '4750', 'Звенигород': '4756', 'Иванов': '4570', 'Ижевск': '4624',
+                  'Иркутск': '4572', 'Йошкар-Ола': '4591', 'Казань': '4618', 'Калининград': '4574', 'Калуга': '4576',
+                  'Кемерово': '4580', 'Киров': '4581', 'Коломна': '4809', 'Королёв': '4813', 'Кострома': '4583',
+                  'Красноармейск': '4817', 'Краснодар': '4584', 'Краснознаменск': '4822', 'Красноярск': '4585',
+                  'Курган': '4586', 'Курск': '4587', 'Кызыл': '4622', 'Липецк': '4589', 'Лобня': '4848',
+                  'Лыткарино': '4851', 'Магадан': '4590', 'Майкоп': '4553', 'Махачкала': '4568', 'Москва': '1',
+                  'Мурманск': '4594', 'Назрань': '4571', 'Нальчик': '4573', 'Нарьян-Мар': '4595', 'Новгород': '4596',
+                  'Новороссийск': '4896', 'Новосибирск': '4598', 'Омск': '4599', 'Оренбург': '4600',
+                  'Орехово-Зуево': '4916', 'Орёл': '4601', 'Пенза': '4602', 'Пермь': '4603', 'Петрозаводск': '4579',
+                  'Петропавловск-Камчатский': '4577', 'Подольск': '4935', 'Протвино': '4945', 'Псков': '4605',
+                  'Пущино': '4949', 'Реутов': '4958', 'Ростов-На-Дону': '4606', 'Рошаль': '4960', 'Рязань': '4607',
+                  'Салехард': '4635', 'Самара': '4608', 'Санкт-Петербург': '2', 'Саранск': '4592', 'Саратов': '4609',
+                  'Серпухов': '4983', 'Смоленск': '4614', 'Сочи': '4998', 'Ставрополь': '4615', 'Сургут': '5003',
+                  'Сыктывкар': '4582', 'Тамбов': '4617', 'Тверь': '4619', 'Тольятти': '5015', 'Томск': '4620',
+                  'Тула': '4621', 'Тюмень': '4623', 'Улан-Удэ': '4563', 'Ульяновск': '4625', 'Уфа': '4560',
+                  'Фрязино': '5038', 'Хабаровск': '4627', 'Ханты-Мансийск': '4629', 'Химки': '5044',
+                  'Чебоксары': '4633', 'Челябинск': '4630', 'Череповец': '5050', 'Черкесск': '4578', 'Чита': '4720',
+                  'Электросталь': '5064', 'Элиста': '4575', 'Южно-Сахалинск': '4611', 'Якутск': '4610',
+                  'Ярославль': '4636'}
+        try:
+            link = "https://www.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=flat&p={}" + \
+                   f"&region={cities[self.region.capitalize()]}"""
+            print(int(self.rooms))
+            if 0 >= int(self.rooms) or int(self.rooms) > 6:
+                raise TypeError
+        except KeyError:
+            raise NameError
+        except ValueError:
+            if 0 >= int(self.rooms[0]) > 6 or 0 >= int(self.rooms[-1]) > 6:
+                raise TypeError
+
+        if len(self.rooms) == 3:
+            for i in range(int(self.rooms[0]), int(self.rooms[-1]) + 1):
+                link += f"&room{i}=1"
+        else:
+            link += f"&room{int(self.rooms)}=1"
+
+        if self.min_price and self.max_price and self.min_price < self.max_price:
+            link += f"&minprice={self.min_price}" + f"&maxprice={self.max_price}"
+        elif self.min_price:
+            link += f"&minprice={self.min_price}"
+        elif self.max_price:
+            link += f"&maxprice={self.max_price}"
+
+
+
+        search_page = self.session.get(link.format(self.page))
+        search_page = search_page.text
+        search_page = BeautifulSoup(search_page, 'lxml')
+        offers = search_page.select("article[data-name='CardComponent']")
+
+        for ind, blocks in enumerate(offers):
+            self.parse_listing(offer=blocks)
+
+        return self.offers
+
+
+def start(username: str, minprice: int, maxprice: int, pages: int, region: str = "Москва", rooms: str = "1-6", ):
+    with open(f'{username}.json', 'w') as file:
+        result = []
+        for i in range(1, pages + 1):
+            print(region, rooms)
+            request = Parser(region=region, rooms=rooms, page=i, minprice=minprice, maxprice=maxprice)
+            result += request.start_session()
+        json.dump(result, file)
+    return True
---
+++ tg_bot.py
@@ -0,0 +1,142 @@
+import logging
+from telegram.ext import CommandHandler, MessageHandler, filters, Application
+import cian_parser
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+TOKEN = '6166579766:AAEfN9Fz8SMY05iyVCtq5VPG6Xoa6AOnv5U'
+application = Application.builder().token(TOKEN).build()
+logger = logging.getLogger(__name__)
+
+user_info = {"username": "", "city": "Москва", "rooms": "1-6", "pages": 2, "min_price": 0, "max_price": 0}
+
+
+async def send_file(update, context):
+    print(user_info["username"])
+
+    chat_id = update.message.chat_id
+
+    file_path = f'{user_info["username"]}.json'
+
+    with open(file_path, 'rb') as file:
+        await context.bot.send_document(chat_id=chat_id, document=file, caption="Готово!")
+
+
+async def parse(update, context):
+    global user_info
+    user = update.message.from_user
+    username = user.username
+    user_info["username"] = username
+    await update.message.reply_text("Подождите, ведётся парсинг...")
+    try:
+
+        if user_info["city"] == "" and user_info["rooms"] == "":
+            cian_parser.start(username=username, pages=user_info["pages"], minprice=user_info["min_price"], maxprice=user_info["max_price"])
+        elif user_info["city"] == "":
+            cian_parser.start(username=username, pages=user_info["pages"], rooms=user_info["rooms"], minprice=user_info["min_price"], maxprice=user_info["max_price"])
+        elif user_info["rooms"] == "":
+            cian_parser.start(username=username, pages=user_info["pages"], region=user_info["city"], minprice=user_info["min_price"], maxprice=user_info["max_price"])
+        else:
+            cian_parser.start(username=username, pages=user_info["pages"], region=user_info["city"], rooms=user_info["rooms"], minprice=user_info["min_price"], maxprice=user_info["max_price"])
+        await send_file(update, context)
+
+    except NameError:
+        await update.message.reply_text("Вы ввели город которого нет в реестре. Можете попробовать ввести ближайший "
+                                        "крупный к вам город.")
+    except TypeError as e:
+        await update.message.reply_text("Вы ввели недопустимое количество комнат. Введите количество комнат от 1 до 6", e)
+
+
+async def settings(update, context):
+    await update.message.reply_text(f"Вот твои настройки сейчас:\nГород: {user_info['city']}\n"
+                                    f"Комнаты: {user_info['rooms']}\nКоличество страниц: {user_info['pages']}\n"
+                                    f"Минимальная цена: {user_info['min_price']}\nМаксимальная цена: "
+                                    f"{user_info['max_price']}")
+
+
+async def start(update, context):
+    await update.message.reply_text(
+        'Привет! Я бот, написанный для предоставления мною данных о недвижимости с сайта cian.ru, о том как мной '
+        'пользоваться в команде /help!')
+
+
+async def min_price(update, context):
+    minprice = context.args[0]
+    user_info["min_price"] = minprice
+    await update.message.reply_text("Минимальная цена сохранена. Напишите /minprice 0 чтобы сбросить.")
+
+
+async def max_price(update, context):
+    maxprice = context.args[0]
+    user_info["max_price"] = maxprice
+    await update.message.reply_text("Максимальная цена сохранена. Напишите /maxprice 0 чтобы сбросить.")
+
+
+async def city(update, context):
+    user_info["city"] = context.args[0]
+    await update.message.reply_text('Ваш город установлен.')
+
+
+async def rooms(update, context):
+    user_info["rooms"] = context.args[0]
+    await update.message.reply_text('Ваше желаемое количество комнат установлено. Чтобы сбросить введите /rooms 1-6.')
+
+
+async def pages(update, context):
+    user_info["pages"] = int(context.args[0])
+    await update.message.reply_text('Ваше количество страниц установлено.')
+
+
+async def help(update, context):
+    await update.message.reply_text('Для начала тебе нужно задать мне настройки:\n'
+                                    '/city <город> - указываешь город, в котором хочешь посмотреть недвижимость (по умолчанию Москва)\n'
+                                    '/rooms <количество комнат> - количество комнат, которое хочешь в квартире  \n'
+                                    '/pages <целое число> - количество страниц, которое вы хотите получить в итоге '
+                                    'парсинга (по умолчанию выводит одну страницу). Не вводите слишком много страниц, '
+                                    'а то обработка будет слишком долгой :)\n'
+                                    'Возможно поставить фильтр цены:\n'
+                                    '/minprice <целое число рублей> - минимальная нужная вам цена.\n '
+                                    '/maxprice <целое число рублей> - максимальная нужная вам цена.\n'
+                                    '/settings - текущие настройки\n'
+                                    'После установки настроек ты можешь запустить парсер командой /parse.\n'
+                                    'Получить данные можно в виде json-файла.\n\n'
+                                    'Если вам надо вернуть поиск любого количества комнат, введите /rooms 1-6.\n'
+                                    'Отключить фильтр цены возможно присвоив ему значение 0\n'
+                                    'Пример: /city Анапа\n/rooms 3\n'
+                                    'P.S.: если вы введете минимальное значение цены больше максимального - фильтр просто не сработает\n'
+                                    'P.S.1: количество комнат строго одна цифра (от 1 до 6)')
+
+
+async def unknown(update, context):
+    """Обработчик неизвестных команд."""
+    await update.message.reply_text('Извините, я не знаю такой команды.')
+
+
+def main() -> None:
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    application.add_handler(CommandHandler('settings', settings))
+
+    application.add_handler(CommandHandler('help', help))
+
+    application.add_handler(CommandHandler('city', city))
+
+    application.add_handler(CommandHandler('pages', pages))
+
+    application.add_handler(CommandHandler('minprice', min_price))
+
+    application.add_handler(CommandHandler('maxprice', max_price))
+
+    application.add_handler(CommandHandler('parse', parse))
+
+    application.add_handler(CommandHandler('rooms', rooms))
+
+    application.add_handler(MessageHandler(filters.COMMAND, unknown))
+
+    application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
